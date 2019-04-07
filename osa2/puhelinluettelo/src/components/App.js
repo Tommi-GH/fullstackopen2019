75dfc42eb349b73aa3e326d3874aefb96e1f126c
Tommi Tiira <tommi.tiira@gmail.com>
2019-04-07 17:16:01 +0300

    2.19 - 2.20*

--- osa2/puhelinluettelo/src/components/App.js
+++ osa2/puhelinluettelo/src/components/App.js
@@ -3,28 +3,32 @@ import PersonList from './Personlist';
 import SearchElement from './SearchElement';
 import AddPerson from './AddPerson';
 import personsService from '../services/Persons'
+import Notification from './Notification'
 
 const App = () => {
     const [newName, setNewName] = useState('')
     const [newPhone, setNewPhone] = useState('')
     const [persons, setPersons] = useState([])
     const [searchTerm, setSearchTerm] = useState('')
+    const [{ message, messageType }, setMessage] = useState({ message: null, messageType: null })
+
     const searchList = (searchTerm.length > 0) ? persons.filter(person =>
         person.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
     ) : persons
 
     useEffect(() => {
-        personsService.getAll().then(returnedPersons => 
+        personsService.getAll().then(returnedPersons =>
             setPersons(returnedPersons))
-        }, [])
+    }, [])
 
     const saveForm = (event) => {
         event.preventDefault()
+
         if (newName.length === 0 || newPhone.length === 0) {
             alert("Täytä molemmat kentät")
             return
         }
-        
+
         if (persons.find(person => person.phone.localeCompare(newPhone) === 0)) {
             alert(`${newPhone} on jo käytössä`)
             return
@@ -34,22 +38,38 @@ const App = () => {
 
         if (foundPerson) {
             const confirm = window.confirm(`${newName} on jo luettelossa, korvataanko numero uudella?`)
-            if(!confirm){ return }
-            const updatePerson = { ...foundPerson, phone: newPhone }
-            
-            personsService.update(updatePerson).then(responsePerson =>
-                setPersons(persons.map(person => responsePerson.id !== person.id ? person : responsePerson))
-            )
-            
-        } else{    
-            personsService.create(newName, newPhone).then(responsePerson =>
-                setPersons(persons.concat(responsePerson))
-            )
+            if (!confirm) { return }
+
+            updatePerson(foundPerson)
+        } else {
+            createPerson()
         }
 
         setNewName('')
         setNewPhone('')
         setSearchTerm('')
+        messagesTimeOut()
+    }
+
+    const updatePerson = (foundPerson) => {
+        const updatedPerson = { ...foundPerson, phone: newPhone }
+
+        personsService.update(updatedPerson).then(responsePerson => {
+            setPersons(persons.map(person => responsePerson.id !== person.id ? person : responsePerson))
+            setMessage({ message: `Henkilön ${responsePerson.name} numero päivitetty: ${responsePerson.phone}!`, messageType: 'success' })
+        }).catch(error => {
+            setPersons(persons.filter(person => person.id !== foundPerson.id))
+            setMessage({ message: `Henkilö ${foundPerson.name} oli jo poistettu palvelimelta`, messageType: 'error' })
+        })
+    }
+
+    const createPerson = () => {
+        personsService.create(newName, newPhone).then(responsePerson => {
+            setPersons(persons.concat(responsePerson))
+            setMessage({ message: `Henkilö ${newName} lisätty!`, messageType: 'success' })
+        }).catch(error => {
+            setMessage({ message: `Virhe luotaessa henkilöä ${newName}`, messageType: 'error' })
+        })
     }
 
     const handleNameChange = (event) => {
@@ -64,25 +84,36 @@ const App = () => {
         setSearchTerm(event.target.value)
     }
 
-    const handleRemove = (personId) => {
+    const removePerson = (personId) => {
+        const foundPerson = persons.find(person => person.id === personId)
+        const confirm = window.confirm(`Poistetaanko ${foundPerson.name}`)
+        if (!confirm) { return }
 
-        const confirm = window.confirm(`Poistetaanko ${persons.find(person => person.id === personId).name}`)
-        if(!confirm){ return }
-
-        personsService.remove(personId).then(
+        personsService.remove(personId).then(reponse => {
             setPersons(persons.filter(person => person.id !== personId))
-        )
-        
+            setMessage({ message: `Henkilö ${foundPerson.name} poistettu!`, messageType: 'success' })
+        }).catch(error => {
+            setPersons(persons.filter(person => person.id !== personId))
+            setMessage({ message: `Henkilö ${foundPerson.name} oli jo poistettu palvelimelta`, messageType: 'error' })
+        })
+        messagesTimeOut()
     }
+
+    const messagesTimeOut = () => (
+        setTimeout(() => {
+            setMessage({ message: null, messageType: null })
+        }, 5000)
+    )
 
 
     return (
         <div>
             <h2>Puhelinluettelo</h2>
+            <Notification message={message} messageType={messageType} />
             <SearchElement searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
             <AddPerson saveForm={saveForm} newName={newName} handleNameChange={handleNameChange}
                 newPhone={newPhone} handlePhoneChange={handlePhoneChange} />
-            <PersonList persons={searchList} removePerson={handleRemove} />
+            <PersonList persons={searchList} removePerson={removePerson} />
         </div>
     )
 
