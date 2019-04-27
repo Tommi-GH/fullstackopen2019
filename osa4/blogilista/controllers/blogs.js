3368cda3e267f43ea6fa9c4228b37d1ac6aaae02
Tommi Tiira <tommi.tiira@gmail.com>
2019-04-27 12:26:11 +0300

    task4.14

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -8,6 +8,17 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+    console.log('get blog', request.params.id)
+    const blog = await Blog.findById(request.params.id)
+    if (!blog) {
+        response.status(404).send()
+        return
+    }
+
+    response.json(blog)
+})
+
 blogsRouter.post('/', async (request, response) => {
     const blog = new Blog({
         title: request.body.title,
@@ -17,11 +28,29 @@ blogsRouter.post('/', async (request, response) => {
     })
 
     if (blog.title === undefined || blog.url === undefined) {
-        response.status(400).json({ error: 'no title or url in blog' })
-    } else {
-        const result = await blog.save()
-        response.status(201).json(result)
+        response.status(400).json({ error: 'no title or url in blog' }).send()
+        return
     }
+    const result = await blog.save()
+    response.status(201).json(result)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+    const updatedBlog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    const blog = await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
+    if (!blog) {
+        response.status(404).send()
+        return
+    }
+    console.log('updated', blog)
+    response.json(blog)
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -5,74 +5,154 @@ const mongoose = require('mongoose')
 const Blog = require('../models/Blog')
 const helperBlogs = require('../misc/blogs_for_testing').blogs
 
-beforeEach(async () => {
-    console.log('removing all')
-    await Blog.deleteMany({})
-    console.log('adding', helperBlogs.length, 'blogs')
+describe('when there is initially some blogs saved', () => {
+    beforeEach(async () => {
+        console.log('removing all')
+        await Blog.deleteMany({})
+        console.log('adding', helperBlogs.length, 'blogs')
 
-    const blogs = helperBlogs
-        .map(blog => new Blog(blog))
-    const promiseArray = blogs.map(blog => blog.save())
-    await Promise.all(promiseArray)
+        const blogs = helperBlogs
+            .map(blog => new Blog(blog))
+        const promiseArray = blogs.map(blog => blog.save())
+        await Promise.all(promiseArray)
 
-    console.log('added', promiseArray.length, 'items')
+        console.log('added', promiseArray.length, 'items')
+    })
+
+    test('blogs as json returned from API', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('all blogs are returned from API', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body.length).toBe(helperBlogs.length)
+    })
+
+    test('identifier id returned from API', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+    })
 })
 
-test('blogs as json returned from API', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('getting single blogs', () => {
+    test('single blog with matching id returned from API', async () => {
+        const response = await api.get('/api/blogs/5a422a851b54a676234d17f7')
+        expect(response.body).toEqual({
+            id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 7,
+        })
+    })
+
+    test('no matching id returns 404 from API', async () => {
+        await api
+            .get('/api/blogs/5a422a851b54a676234d17f1')
+            .expect(404)
+    })
 })
 
-test('all blogs are returned from API', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body.length).toBe(helperBlogs.length)
+describe('adding new blogs', () => {
+    test('post added via API', async () => {
+        const addBlog = {
+            title: 'Go To Statement Considered Harmful',
+            likes: '5',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html'
+        }
+        await api.post('/api/blogs')
+            .send(addBlog)
+            .expect(201)
+
+        const response = await api.get('/api/blogs')
+        expect(response.body.length).toBe(helperBlogs.length + 1)
+    })
+
+    test('post added without likes returns likes 0 API', async () => {
+        const addBlog = {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html'
+        }
+
+        await api.post('/api/blogs')
+            .send(addBlog)
+            .expect(201)
+
+        const response = await api.get('/api/blogs')
+        expect(response.body[response.body.length - 1].likes).toBe(0)
+    })
+
+
+    test('post added without title and url returns 400 API', async () => {
+        const addBlog = {
+            likes: 10,
+            author: 'Edsger W. Dijkstra'
+        }
+
+        await api.post('/api/blogs')
+            .send(addBlog)
+            .expect(400)
+
+    })
 })
 
-test('identifier id returned from API', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].id).toBeDefined()
+describe('removing blogs', () => {
+    test('delete blog with valid id is removed from db API', async () => {
+        await api.delete('/api/blogs/5a422a851b54a676234d17f7')
+            .expect(204)
+
+        const response = await api.get('/api/blogs')
+        expect(response.body.length).toBe(helperBlogs.length - 1)
+    })
+
+    test('delete blog with invalid id does not affect db API', async () => {
+        await api.delete('/api/blogs/5a422a851b54a676234d17f1')
+            .expect(204)
+
+        const response = await api.get('/api/blogs')
+        expect(response.body.length).toBe(helperBlogs.length)
+    })
 })
 
-test('post added via API', async () => {
-    const addBlog = helperBlogs[0]
-    await api.post('/api/blogs')
-        .send(addBlog)
-        .expect(201)
+describe('editing blogs', () => {
+    test('adding 1 like to blog with valid id is saved to db API', async () => {
+        const updateBlog = {
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 5,
+        }
+        await api.put('/api/blogs/5a422a851b54a676234d17f7')
+            .send(updateBlog)
+            .expect(200)
 
-    const response = await api.get('/api/blogs')
-    expect(response.body.length).toBe(helperBlogs.length + 1)
+        const response = await api.get('/api/blogs/5a422a851b54a676234d17f7')
+        expect(response.body).toEqual({
+            id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 5,
+        })
+    })
+
+    test('adding 1 like to blog with invalid id does not affect db API', async () => {
+        const updateBlog = {
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 5,
+        }
+        await api.put('/api/blogs/5a422a851b54a676234d17f1')
+            .send(updateBlog)
+            .expect(404)
+    })
 })
-
-test('post added without likes returns likes 0 API', async () => {
-    const addBlog = {
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html'
-    }
-
-    await api.post('/api/blogs')
-        .send(addBlog)
-        .expect(201)
-
-    const response = await api.get('/api/blogs')
-    expect(response.body[response.body.length-1].likes).toBe(0)
-})
-
-
-test('post added without title and url returns 400 API', async () => {
-    const addBlog = {
-        likes: 10,
-        author: 'Edsger W. Dijkstra'
-    }
-
-    await api.post('/api/blogs')
-        .send(addBlog)
-        .expect(400)
-
-})
-
 
 afterAll(() => {
     mongoose.connection.close()
