117de92650dcd242cd193eb7c6f7d839b82db484
TommiTiira <tommi.tiira@citrus.fi>
2019-05-20 16:49:58 +0300

    task5.13-5.14 & started 5.15.

---
+++ osa5/bloglist-frontend/src/tests/SimpleBlog.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import 'jest-dom/extend-expect'
+import { render, cleanup, findAllByTestId, fireEvent } from 'react-testing-library'
+import SimpleBlog from '../components/SimpleBlog'
+
+afterEach(cleanup)
+
+test('render title, author & likes', () => {
+    const blog = {
+        title: 'This is a title',
+        author: 'A.U. Thor',
+        likes: '27'
+    }
+
+    const component = render(<SimpleBlog blog={blog} />)
+
+    expect(component.container).toHaveTextContent('This is a title')
+    expect(component.container).toHaveTextContent('A.U. Thor')
+    expect(component.container).toHaveTextContent('blog has 27 likes')
+})
+
+test('clicking the like button twice calls the function twice', () => {
+        const blog = {
+            title: 'This is a title',
+            author: 'A.U. Thor',
+            likes: '27'
+        }
+
+        const mockHandler = jest.fn()
+
+        const {getByText} = render(<SimpleBlog blog={blog} onClick={mockHandler} />)
+        const button = getByText('like')
+
+        fireEvent.click(button)
+        fireEvent.click(button)
+
+        expect(mockHandler.mock.calls.length).toBe(2)
+    })
